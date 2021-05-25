09dfb51721b90a4f334abd62e50053b52300bdb3
raul <rausanlo@epsg.upv.es>
2021-05-26 01:17:24 +0200

    prueba inicial

--- driver-i2c-between/master-esp32-i2c/master-esp32-i2c.ino
+++ driver-i2c-between/master-esp32-i2c/master-esp32-i2c.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <Wire.h>
+
+void setup()
+{
+  Serial.begin(9600); 
+  delay(3000);
+  Serial.print("Inicio del master i2c en esp 32 {1,-2,5,1,8,2,4,10}");
+  Wire.begin(); // join i2c bus (address optional for master)
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+byte x = 0;
 
+void loop()
+{
+  //el esp como master envia la imagen 
+  Wire.beginTransmission(4); // transmit to device #4
+  Wire.write("{ 1,2,3,4,5,6 }");        // sends five bytes
+  Wire.endTransmission();    // stop transmitting
+
+  x++;
+  delay(5000);
 }
--- driver-i2c-between/slave-opencr-i2c/slave-opencr-i2c.ino
+++ driver-i2c-between/slave-opencr-i2c/slave-opencr-i2c.ino
@@ -5,17 +5,21 @@ void setup()
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);           // start serial for output
+  delay(3000);
+  Serial.print("inicio de i2c en open cr");
 }
 
 void loop()
 {
-  delay(100);
+  delay(1000);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany)
 {
+  Serial.println("HowMany: ");  
+  Serial.println(howMany);
   while(1 < Wire.available()) // loop through all but the last
   {
     char c = Wire.read(); // receive byte as a character
