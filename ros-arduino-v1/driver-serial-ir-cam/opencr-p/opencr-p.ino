eb42cccc6703d91b989b42b70c8fe8b01b044d06
raul <rausanlo@epsg.upv>
2021-06-08 22:19:56 +0200

    mejoras

--- ros-arduino-v1/driver-serial-ir-cam/opencr-p/opencr-p.ino
+++ ros-arduino-v1/driver-serial-ir-cam/opencr-p/opencr-p.ino
@@ -1,7 +1,7 @@
 
-const byte numChars = 1000;
-char receivedChars[numChars];
-
+const byte numChars = 3000;
+String receivedChars="";
+boolean recvInProgress = false;
 boolean newData = false;
 int num = 0;
 
@@ -13,26 +13,37 @@ void setup() {
 }
 
 void loop() {
-    delay(7000);
-    Serial.println("esto es loop");
-    getImage();
-    delay(50);  
+  if(recvInProgress == false){
+      delay(7000);
+      Serial.println("esto es loop");
+      getImage();
+      delay(1);  
+  }
     recvWithStartEndMarkers();
     showNewData();
+    delay(1);
 }
 
 void getImage(){ 
-    num++;
-    Serial.println("peticion de frame ");
-    Serial.println(num);
-    Serial2.write("<IR>");
-    delay(10);
+    if(recvInProgress == false){
+      receivedChars="";
+      num++;
+      Serial.println("peticion de frame ");
+      Serial.println(num);
+      Serial2.write("<IR>");
+      delay(1);
+    }
     recvWithStartEndMarkers();
     showNewData();
+    if(recvInProgress==true){
+      Serial.println("rev");
+    }
+    
+    delay(1);
 }
 
 void recvWithStartEndMarkers() {
-    static boolean recvInProgress = false;
+    
     static byte ndx = 0;
     char startMarker = '<';
     char endMarker = '>';
@@ -40,17 +51,17 @@ void recvWithStartEndMarkers() {
  
     while (Serial2.available() > 0 && newData == false) {
         rc = Serial2.read();
-
+        //Serial.print(rc);
         if (recvInProgress == true) {
             if (rc != endMarker) {
-                receivedChars[ndx] = rc;
+                receivedChars = receivedChars + rc;
                 ndx++;
                 if (ndx >= numChars) {
                     ndx = numChars - 1;
                 }
             }
             else {
-                receivedChars[ndx] = '\0'; // terminate the string
+                //receivedChars[ndx] = '\0'; // terminate the string
                 recvInProgress = false;
                 ndx = 0;
                 newData = true;
